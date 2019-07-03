c769a181cfd32077266e0286171ce2a0d88ef1f8
Nathan <nathanjwtx@nathanjwtx.io>
2019-07-03 14:57:08 -0500

    Initial project setup and code push

---
+++ SortAndTransform/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SortAndTransform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {97, 2, 25, 24};
+            Console.WriteLine(SortAndTransform(numbers));
+        }
+
+        private static string SortAndTransform(int[] numbers)
+        {
+            string result = string.Empty;
+            string stringOne = String.Empty;
+
+            result = Convert.ToChar(numbers[0]).ToString();
+
+            return result;
+        }
+    }
+}
